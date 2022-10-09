160b51bc5f697eca806cf922ac9cea426f146c81
Peng Chen <1533431376@qq.com>
2022-10-09 13:48:46 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,10 +6,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1878025-45b567829dfb33489b2f7f14',
+    //'m784490063-7b5da437e7f1e0d67613714d',
+    //'m784497419-de55aa09902ccb3ab22d548a',
+    //'m784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 日志天数
