f88b847e6564c73df2159b54a822ebea810b5548
peng <1533431376@qq.com>
2022-10-21 12:36:49 +0800

    add https://cpen.top:8004/

--- public/config.js
+++ public/config.js
@@ -7,7 +7,7 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur1878025-45b567829dfb33489b2f7f14',
-    //'m784490063-7b5da437e7f1e0d67613714d',
+    'm792901070-6d1c5db460255383a33b3120',
     //'m784497419-de55aa09902ccb3ab22d548a',
     //'m784496436-71a4bf7b1e3bdf7756be131b',
   ],
